b65620f54b9c8918c7244c5772c310cf82c12a90
Ameer Arsala <ameer.arsala03@gmail.com>
2024-11-15 15:09:42 -0800

    initial stable

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # blendctl
+
+## How to Run
+
+1. Clone repo
+2. `source ./profile.sh`
+3. `hostexec bash -c <cmd to execute>` or `hostexec zsh -c <cmd to execute>`
+
+Additionally, for stuff from the `.bashrc` or `.zshrc` use the `-i` flag (means interactive) for the shell exec. Examples: `hostexec bash -ic <cmd to execute>` or `hostexec zsh -ic <cmd to execute>`
+
--- profile.sh
+++ profile.sh
@@ -8,9 +8,11 @@ hostexec() {
   container_host_exec_cache_dir_file="$container_host_exec_cache_dir/host_dir"
 
   mkdir -p "$container_host_exec_cache_dir"
-  CONTAINER_HOST_EXEC_CACHE_DIR_FILE=container_host_exec_cache_dir_file ./container_host_exec "$@"
+
+  # TODO: remove the `./` from `./container-host-exec`. It's only temporary for development
+  CONTAINER_HOST_EXEC_CACHE_DIR_FILE=$container_host_exec_cache_dir_file ./container-host-exec "$@"
 
   # cleanup
-  builtin cd -- "$(cat "$container_host_exec_cache_dir_file")"
+  builtin cd -- "$(cat "$container_host_exec_cache_dir_file")" || exit
   rm -f "$container_host_exec_cache_dir_file"
 }
